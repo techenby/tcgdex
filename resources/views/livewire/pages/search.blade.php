fcd792781afddfe8d57046e731eaba29231afb09
Andy Newhouse <hi@andymnewhouse.me>
2024-06-19 20:56:34 -0500

    Add pagination

--- resources/views/livewire/pages/search.blade.php
+++ resources/views/livewire/pages/search.blade.php
@@ -4,7 +4,7 @@ use App\Http\Integrations\PokemonTcg\PokemonTcg;
 use App\Http\Integrations\PokemonTcg\Requests\GetCardsRequest;
 use App\Models\Card;
 
-use function Livewire\Volt\{layout, state, usesPagination};
+use function Livewire\Volt\{layout, state, computed, usesPagination};
 
 usesPagination();
 
@@ -12,20 +12,9 @@ layout('layouts.app');
 
 state([
     'query' => '',
-    'cards' => [],
 ]);
 
-$search = function () {
-    if ($this->query === '') {
-        $this->cards = [];
-
-        return;
-    }
-
-    $this->cards = Card::search($this->query)->options([
-        'query_by' => 'name, hp'
-    ])->get();
-};
+$cards = computed(fn () => Card::search($this->query)->paginate(10));
 
 $addToDex = function ($id) {
     auth()->user()->cards()->attach($id);
@@ -37,7 +26,7 @@ $addToDex = function ($id) {
     <div class="py-12 px-4 space-y-8">
         <form wire:submit="search" class="relative max-w-md mx-auto rounded-full ">
             <label for="query" class="sr-only">Search for Cards</label>
-            <x-form.input wire:keyup.debounce="search" wire:model="query" type="text" placeholder="Search by name, HP and more" class="w-full py-4 rounded-full md:text-lg md:py-4 md:px-6" />
+            <x-form.input wire:model.live="query" type="text" placeholder="Search by name, HP and more" class="w-full py-4 rounded-full md:text-lg md:py-4 md:px-6" />
             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-6">
                 <svg xmlns="http://www.w3.org/2000/svg" class="size-7 stroke-gray-700 dark:stroke-gray-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"/>
@@ -46,8 +35,9 @@ $addToDex = function ($id) {
         </form>
 
 
+        @if ($query !== '')
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
-            @foreach ($cards as $card)
+            @foreach ($this->cards as $card)
             <div id="{{ $card->external_id }}" class="group relative">
                 <img src="{{ $card->images['small'] }}" alt="{{ $card->name }} - {{ $card->flavor_text ?? '' }}" class="shadow rounded-xl">
                 <div class="sr-only">
@@ -64,5 +54,7 @@ $addToDex = function ($id) {
             </div>
             @endforeach
         </div>
+        {{ $this->cards->links() }}
+        @endif
     </div>
 </div>
