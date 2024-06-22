791b584f3ea81cd9ba751f98485c98ad857dff6a
Andy Newhouse <hi@andymnewhouse.me>
2024-06-21 20:04:52 -0500

    Add deck model

---
+++ app/Models/Deck.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Deck extends Model
+{
+    use HasFactory;
+
+    public function cards(): BelongsToMany
+    {
+        return $this->belongsToMany(Card::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -36,6 +37,11 @@ class User extends Authenticatable
     public function cards(): BelongsToMany
     {
         return $this->belongsToMany(Card::class)->withPivot('id')->withTimestamps();
+    }
+
+    public function decks(): HasMany
+    {
+        return $this->hasMany(Deck::class);
     }
 
     /**
