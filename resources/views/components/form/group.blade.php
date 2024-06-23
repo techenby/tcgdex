b6ea72753b915e426768d9609ebde72211e06d1e
Andy Newhouse <hi@andymnewhouse.me>
2024-06-23 11:58:32 -0500

    Add textarea

--- resources/views/components/form/group.blade.php
+++ resources/views/components/form/group.blade.php
@@ -6,6 +6,10 @@
 
 <div>
     <x-form.label :for="$id" :value="__($label)" />
+    @if ($attributes->get('type') === 'textarea')
+    <x-dynamic-component :component="'form.' . $attributes->get('type')" {{ $attributes->twMerge(['class' => 'block mt-1 w-full']) }} />
+    @else
     <x-form.input {{ $attributes->merge(['class' => 'block mt-1 w-full' , 'type' => 'text']) }} />
+    @endif
     <x-form.error :messages="$error" class="mt-2" />
 </div>
