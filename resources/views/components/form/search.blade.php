0762e2ef11094da11443bea68a9b004d21a7cb46
Andy Newhouse <hi@andymnewhouse.me>
2024-06-23 12:33:17 -0500

    Remove commented out

--- resources/views/components/form/search.blade.php
+++ resources/views/components/form/search.blade.php
@@ -6,7 +6,4 @@
         </svg>
     </div>
     <input type="search" name="query" wire:model.live="query" id="query" placeholder="Find by name, hp or attack" class="block w-full rounded-full border-0 py-2 pr-4 pl-9 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:leading-6 dark:bg-white/5 dark:text-zinc-400 dark:ring-inset dark:ring-white/10 dark:hover:ring-white/20">
-    <!-- <div class="absolute inset-y-0 right-0 flex py-2 pr-2">
-        <kbd class="inline-flex items-center rounded-full border border-gray-200 px-2 font-sans text-xs text-gray-400">⌘K</kbd>
-    </div> -->
 </div>
