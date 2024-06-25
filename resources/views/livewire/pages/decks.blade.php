a2b054dba4e08c2a965fa149577396a046b6434b
Andy Newhouse <hi@andymnewhouse.me>
2024-06-24 21:23:35 -0500

    Move from to collapsable panel

--- resources/views/livewire/pages/decks.blade.php
+++ resources/views/livewire/pages/decks.blade.php
@@ -29,48 +29,60 @@ $delete = function ($id) {
 
 ?>
 
-<x-ui.container class="space-y-8">
-    <form wire:submit="create" class="space-y-4 bg-white dark:bg-gray-800 p-4 rounded-md shadow max-w-md">
-        <h2 class="text-lg text-gray-900 dark:text-gray-100">Add Deck</h2>
-        <x-form.group name="name" wire:model="form.name" type="text" />
-        <x-form.group name="notes" wire:model="form.notes" type="textarea" />
+<div>
+    <x-ui.container class="mb-16">
+        <x-table>
+            <x-slot:thead>
+                <tr>
+                    <x-table.th>Name</x-table.th>
+                    <x-table.th>Pokémon</x-table.th>
+                    <x-table.th>Trainer</x-table.th>
+                    <x-table.th>Energy</x-table.th>
+                    <x-table.th>Total</x-table.th>
+                    <x-table.th>
+                        <span class="sr-only">Edit</span>
+                    </x-table.th>
+                </tr>
+            </x-slot:thead>
+            <x-slot:tbody>
+                @forelse ($this->decks as $deck)
+                <tr>
+                    <x-table.td>{{ $deck->name }}</x-table.td>
+                    <x-table.td>{{ $deck->cards->where('supertype', 'Pokémon')->count() }}</x-table.td>
+                    <x-table.td>{{ $deck->cards->where('supertype', 'Trainer')->count() }}</x-table.td>
+                    <x-table.td>{{ $deck->cards->where('supertype', 'Energy')->count() }}</x-table.td>
+                    <x-table.td>{{ $deck->cards->count() }}</x-table.td>
+                    <x-table.td class="space-x-2">
+                        <a href="{{ route('decks.edit', $deck) }}">Edit</a>
+                        <button type="button" wire:click="delete({{ $deck->id }})">
+                            Delete
+                        </button>
+                    </x-table.td>
+                </tr>
+                @empty
+                <tr>
+                    <x-table.td colspan="6">No decks</x-table.td>
+                </tr>
+                @endforelse
+            </x-slot:tbody>
+        </x-table>
+    </x-ui.container>
 
-        <x-primary-button>Save</x-primary-button>
-    </form>
+    <div class="fixed bottom-0 w-full">
+        <x-ui.container class="max-w-md" without-y-padding>
+            <div x-data="{expanded: false}" @class(['min-h-8 bg-white dark:bg-gray-800 dark:border dark:border-gray-700 px-8 rounded-t-2xl drop-shadow-3xl relative'])>
+                <button @click="expanded = ! expanded" class="z-10 absolute -top-4 shadow bg-white px-3 py-2 dark:text-gray-100 dark:bg-gray-800 text-sm dark:border dark:border-gray-700 rounded-full">
+                    Add New Deck
+                </button>
+                <div x-show="expanded" x-collapse class="space-y-8 max-h-[50vh] overflow-scroll pb-8 pt-8 -mx-8 px-8">
+                    <form wire:submit="create" class="space-y-4">
+                        <x-form.group name="name" wire:model="form.name" type="text" />
+                        <x-form.group name="notes" wire:model="form.notes" type="textarea" />
 
-    <x-table>
-        <x-slot:thead>
-            <tr>
-                <x-table.th>Name</x-table.th>
-                <x-table.th>Pokémon</x-table.th>
-                <x-table.th>Trainer</x-table.th>
-                <x-table.th>Energy</x-table.th>
-                <x-table.th>Total</x-table.th>
-                <x-table.th>
-                    <span class="sr-only">Edit</span>
-                </x-table.th>
-            </tr>
-        </x-slot:thead>
-        <x-slot:tbody>
-            @forelse ($this->decks as $deck)
-            <tr>
-                <x-table.td>{{ $deck->name }}</x-table.td>
-                <x-table.td>{{ $deck->cards->where('supertype', 'Pokémon')->count() }}</x-table.td>
-                <x-table.td>{{ $deck->cards->where('supertype', 'Trainer')->count() }}</x-table.td>
-                <x-table.td>{{ $deck->cards->where('supertype', 'Energy')->count() }}</x-table.td>
-                <x-table.td>{{ $deck->cards->count() }}</x-table.td>
-                <x-table.td class="space-x-2">
-                    <a href="{{ route('decks.edit', $deck) }}">Edit</a>
-                    <button type="button" wire:click="delete({{ $deck->id }})">
-                        Delete
-                    </button>
-                </x-table.td>
-            </tr>
-            @empty
-            <tr>
-                <x-table.td colspan="6">No decks</x-table.td>
-            </tr>
-            @endforelse
-        </x-slot:tbody>
-    </x-table>
-</x-ui.container>
+                        <x-primary-button>Save</x-primary-button>
+                    </form>
+                </div>
+            </div>
+        </x-ui.container>
+    </div>
+</div>
