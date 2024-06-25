e28d04098709081bb26ddfe5e26ce9eb27e930ce
Andy Newhouse <hi@andymnewhouse.me>
2024-06-24 21:13:06 -0500

    Add dynamic label

--- resources/views/livewire/search-cards.blade.php
+++ resources/views/livewire/search-cards.blade.php
@@ -5,11 +5,18 @@ use App\Models\Deck;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
-use function Livewire\Volt\{computed, state, usesPagination};
+use function Livewire\Volt\{computed, mount, state, usesPagination};
 
 usesPagination();
 
-state(['model', 'query' => '']);
+state(['label', 'model', 'query' => '']);
+
+mount(function () {
+    $this->label = match (get_class($this->model)) {
+        User::class => 'Add Card to Collection',
+        Deck::class => 'Add Card to Deck',
+    };
+});
 
 $collection = computed(fn () => $this->model->collection());
 $searchedCards = computed(fn () => Card::search($this->query)->paginate(10));
@@ -28,7 +35,9 @@ $sub = function ($pivotId) {
 <div class="fixed bottom-0 w-full">
     <x-ui.container without-y-padding>
         <div x-data="{expanded: false}" @class(['min-h-8 bg-white dark:bg-gray-800 dark:border dark:border-gray-700 px-8 rounded-t-2xl shadow relative'])>
-            <button @click="expanded = ! expanded" class="z-10 absolute -top-4 shadow bg-white px-3 py-2 dark:text-gray-100 dark:bg-gray-800 text-sm dark:border dark:border-gray-700 rounded-full">Add Cards to Collection</button>
+            <button @click="expanded = ! expanded" class="z-10 absolute -top-4 shadow bg-white px-3 py-2 dark:text-gray-100 dark:bg-gray-800 text-sm dark:border dark:border-gray-700 rounded-full">
+                {{ $label }}
+            </button>
             <div x-show="expanded" x-collapse class="space-y-8 max-h-[50vh] overflow-scroll pb-8 pt-8 -mx-8 px-8">
                 <form wire:submit="search" class="relative max-w-md mx-auto rounded-full ">
                     <x-form.search />
